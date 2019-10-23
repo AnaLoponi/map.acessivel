ad0bbb8ee874186aeb74a36b702f8ab2e533b1b3
jooaodias <joaovitordias92040@gmail.com>
2019-10-23 16:12:19 -0300

    Atualizações João

---
+++ scripts/pgts.js
@@ -0,0 +1,17 @@
+(function pgts() {
+  'use strict';
+  window.addEventListener('load', function pgts() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function pgts(form) {
+      form.addEventListener('submit', function pgts(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
